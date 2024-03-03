e991bfa0e12033bb4d0a71f4d5d558a91dfef03a
Selina Song <selinasong6@gmail.com>
2024-03-03 14:57:29 -0800

    homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Homepage from './Homepage';
 function App() {
   return (
+    
+
     <div className="App">
+      <Homepage/>
+      {/* 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+  */}
     </div>
   );
 }
---
+++ src/Homepage.js
@@ -0,0 +1,17 @@
+
+function Homepage() {
+    return (
+      <div className="Homepage">
+        <header className="UniFit-Home">
+          
+          <h1 className="title">swap sustainably</h1> {/* Title */}
+          <p>
+            Welcome to UniFit!
+          </p>
+          <button className="myButton"> Browse our market </button> {/* Button */}
+        </header>
+      </div>
+    );
+  }
+  
+  export default Homepage;
